879dc493ece0fddcd4cf822ed740aa50bb68d6fc
Alex Irpan <xelanapri@gmail.com>
2022-02-03 12:11:34 -0800

    Incrasing word list before doing any more work

--- src/lib/words.ts
+++ src/lib/words.ts
@@ -14,8 +14,11 @@ export const isWinningWord = (word: string) => {
 }
 
 export const getWordOfDay = () => {
+  // A random set of 25 words first (WORDS constant is already randomized)
+  const solutions = WORDS.slice(800, 800+25);
+  // expects uppercase versions of everything
   return {
-    solutions: ['WARDS', 'SWORD', 'FOOLS']
+    solutions: solutions.map(w => w.toUpperCase())
   }
 }
 
