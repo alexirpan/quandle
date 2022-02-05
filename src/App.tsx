3159e67ad32e87593ce640aa00dc5c7c520f7824
Alex Irpan <xelanapri@gmail.com>
2022-02-04 23:28:55 -0800

    Some notes, change number of eyes to 2, move up to 50 words

--- src/App.tsx
+++ src/App.tsx
@@ -23,7 +23,7 @@ function App() {
   const [successAlert, setSuccessAlert] = useState('')
   const [guesses, setGuesses] = useState<string[]>([])
   const [realities, setRealities] = useState<string[]>(solutions)
-  const [eyes, setEyes] = useState<number>(3)
+  const [eyes, setEyes] = useState<number>(2)
 
   const [stats, setStats] = useState(() => loadStats())
 
--- src/lib/words.ts
+++ src/lib/words.ts
@@ -15,7 +15,7 @@ export const isWinningWord = (word: string) => {
 
 export const getWordOfDay = () => {
   // A random set of 25 words first (WORDS constant is already randomized)
-  const solutions = WORDS.slice(800, 800+25);
+  const solutions = WORDS.slice(700, 700+50);
   // expects uppercase versions of everything
   return {
     solutions: solutions.map(w => w.toUpperCase())
